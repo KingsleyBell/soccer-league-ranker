567077b76775bb0f537456542abc5965ccdde22c
Luke Bell <lukekingsleybell@gmail.com>
2020-03-30 13:41:08 +0200

    Add tests for total score calculator and ranking table methods.

--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ranker.main import get_points, get_team_name_and_score
+from ranker.main import calculate_scores, get_points, get_ranking_table, get_team_name_and_score
 
 
 class TestRanker:
@@ -31,3 +31,31 @@ class TestRanker:
 
         assert team1_points == expected_team1_points
         assert team2_points == expected_team2_points
+
+    @pytest.mark.parametrize(
+        'match_scores_string, expected_scores',
+        [
+            ('Team1 1, Team2 0', {"Team1": 3, "Team2": 0}),
+            ('Team1 1, Team2 1\nTeam1 5, Team3 3', {"Team1": 4, "Team2": 1, "Team3": 0}),
+            ('T1 1, T2 0\nT1 5, T3 3\nT2 4, T3 4', {"T1": 6, "T2": 1, "T3": 1}),
+        ]
+    )
+    def test_calculate_scores(self, match_scores_string, expected_scores):
+        scores = calculate_scores(match_scores_string)
+
+        assert scores == expected_scores
+
+    @pytest.mark.parametrize(
+        'scores, expected_ranking_table',
+        [
+            ({'T1': 0, 'T2': 3}, '1. T2, 3 pts\n2. T1, 0 pts'),
+            (
+                    {'T1': 0, 'T2': 3, 'T4': 1, 'T3': 1},
+                    '1. T2, 3 pts\n2. T3, 1 pt\n2. T4, 1 pt\n4. T1, 0 pts'
+            ),
+        ]
+    )
+    def test_get_ranking_table(self, scores, expected_ranking_table):
+        ranking_table = get_ranking_table(scores)
+
+        assert ranking_table == expected_ranking_table
